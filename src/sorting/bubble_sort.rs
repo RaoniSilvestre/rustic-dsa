f41eb96b89f51fe695bde13bd278272e325c9b5e
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-10-22 20:25:52 -0300

    adding recursive BubbleSort

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,18 +1,48 @@
-pub trait BubbleSort<T: PartialOrd + Copy> {
-    fn bubble_sort(&mut self);
-}
+use super::{IterativeBubbleSort, OrderedCopy, RecursiveBubbleSort};
 
-impl<T> BubbleSort<T> for Vec<T>
-where
-    T: PartialOrd + Copy,
-{
-    fn bubble_sort(&mut self) {
-        for i in 0..self.len() {
-            for j in 0..self.len() {
-                if self[i] < self[j] {
-                    self.swap(i, j)
+impl<T: OrderedCopy> IterativeBubbleSort<T> for Vec<T> {
+    fn iterative_bubble_sort(&mut self) {
+        let len = self.len();
+        let mut is_ordered = true;
+
+        for i in 0..len {
+            for j in 0..len - 1 - i {
+                if self[j] > self[j + 1] {
+                    is_ordered = false;
+                    self.swap(j, j + 1)
                 }
+            }
+
+            if is_ordered {
+                break;
             }
         }
     }
+}
+
+impl<T: OrderedCopy> RecursiveBubbleSort<T> for Vec<T> {
+    fn recursive_bubble_sort(&mut self) {
+        let len = self.len();
+        recursive_bubble(self, len)
+    }
+}
+
+fn recursive_bubble<T: OrderedCopy>(vec: &mut Vec<T>, n: usize) {
+    if n == 1 {
+        return;
+    }
+    bubble_pass(vec, 0, n);
+    recursive_bubble(vec, n - 1);
+}
+
+fn bubble_pass<T: OrderedCopy>(vec: &mut Vec<T>, i: usize, j: usize) {
+    if i == j - 1 {
+        return;
+    }
+
+    if vec[i] > vec[i + 1] {
+        vec.swap(i, i + 1);
+    }
+
+    bubble_pass(vec, i + 1, j);
 }
