b73541fac3bcdf64d035a494ffce582fccc15a3d
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-11-24 20:18:20 -0300

    Implementação final de Binary Search Tree

--- src/data_structures/bin_tree/mod.rs
+++ src/data_structures/bin_tree/mod.rs
@@ -1,32 +1,68 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fmt::Debug};
 
-pub trait OrderedCopy: Ord + Copy {}
-impl<T: Ord + Copy> OrderedCopy for T {}
+pub trait OrderedCopy: Ord + Copy + Debug {}
+impl<T: Ord + Copy + Debug> OrderedCopy for T {}
 
 pub type OptionNode<T> = Option<Box<Node<T>>>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Node<OrderedCopy> {
     value: OrderedCopy,
     left: OptionNode<OrderedCopy>,
     right: OptionNode<OrderedCopy>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub struct BinTree<OrderedCopy> {
     root: OptionNode<OrderedCopy>,
 }
 
 impl<T: OrderedCopy> BinTree<T> {
-    pub fn inserir(&mut self, chave: T) {
-        match self.root.as_mut() {
-            Some(node) => node.insert(chave),
-            None => self.root = Node::new_node(chave),
+    pub fn insert(&mut self, chave: T) {
+        Node::insert(&mut self.root, chave)
+    }
+
+    pub fn search(&self, chave: T) -> bool {
+        Node::search(&self.root, chave)
+    }
+
+    pub fn remove(&mut self, chave: T) {
+        Node::remove(&mut self.root, chave);
+    }
+}
+
+// Principal
+impl<T: OrderedCopy> Node<T> {
+    fn insert(node_opt: &mut OptionNode<T>, chave: T) {
+        match node_opt {
+            Some(node) => match chave.cmp(&node.value) {
+                Ordering::Less => Self::insert(&mut node.left, chave),
+                Ordering::Greater => Self::insert(&mut node.right, chave),
+                _ => (),
+            },
+            None => *node_opt = Node::new_node(chave),
         }
     }
 
-    pub fn search(&self, chave: T) -> Option<Node<T>> {
-        self.root.as_ref().and_then(|node| node.search(chave))
+    fn search(node_opt: &OptionNode<T>, chave: T) -> bool {
+        match node_opt {
+            Some(node) => match chave.cmp(&node.value) {
+                Ordering::Equal => true,
+                Ordering::Less => Self::search(&node.left, chave),
+                Ordering::Greater => Self::search(&node.right, chave),
+            },
+            None => false,
+        }
+    }
+
+    fn remove(node_opt: &mut OptionNode<T>, chave: T) {
+        if let Some(node) = node_opt {
+            match chave.cmp(&node.value) {
+                Ordering::Less => Self::remove(&mut node.left, chave),
+                Ordering::Greater => Self::remove(&mut node.right, chave),
+                Ordering::Equal => Self::remove_node(node_opt),
+            }
+        }
     }
 }
 
@@ -41,35 +77,44 @@ impl<T: OrderedCopy> Node<T> {
     }
 }
 
-// Principal
-impl<T: OrderedCopy> Node<T> {
-    pub fn insert(&mut self, chave: T) {
-        match self.value.cmp(&chave) {
-            Ordering::Equal => return,
-            Ordering::Less => Self::insert_on_side(&mut self.left, chave),
-            Ordering::Greater => Self::insert_on_side(&mut self.right, chave),
-        }
-    }
-
-    fn search(&self, chave: T) -> Option<Node<T>> {
-        match self.value.cmp(&chave) {
-            Ordering::Equal => Some(self.clone()),
-            Ordering::Less => Self::search_on_side(&self.left, chave),
-            Ordering::Greater => Self::search_on_side(&self.right, chave),
-        }
-    }
-}
-
 // Auxiliar
 impl<T: OrderedCopy> Node<T> {
-    fn insert_on_side(side: &mut OptionNode<T>, chave: T) {
+    fn find_min(side: &OptionNode<T>) -> OptionNode<T> {
         match side {
-            Some(node) => node.insert(chave),
-            None => *side = Self::new_node(chave),
+            None => None,
+            Some(node) if node.left.is_none() => Some(node.clone()),
+            Some(node) => Self::find_min(&node.left),
         }
     }
 
-    fn search_on_side(side: &OptionNode<T>, chave: T) -> Option<Node<T>> {
-        side.as_ref().and_then(|node| node.search(chave))
+    fn remove_node(node_opt: &mut OptionNode<T>) {
+        if let Some(node) = node_opt {
+            match (node.left.take(), node.right.take()) {
+                (None, None) => {
+                    *node_opt = None;
+                }
+                (None, Some(right_node)) => {
+                    node.value = right_node.value;
+                    node.left = right_node.left;
+                    node.right = right_node.right;
+                }
+                (Some(left_node), None) => {
+                    node.value = left_node.value;
+                    node.left = left_node.left;
+                    node.right = left_node.right;
+                }
+                (Some(left_node), Some(right_node)) => {
+                    let mut min_node = Node::find_min(&mut Some(right_node));
+                    if let Some(min) = min_node.as_mut() {
+                        node.value = min.value;
+                        node.left = Some(left_node);
+                        node.right = min.right.clone();
+                        Node::remove(&mut node.right, node.value);
+                    }
+                }
+            }
+        }
     }
 }
+
+pub mod display;
