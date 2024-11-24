ee402e530a315aad584ecc73ff535b086cb0709c
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-11-24 13:20:21 -0300

    corrigindo testes unitários

--- tests/sorting_tests.rs
+++ tests/sorting_tests.rs
@@ -1,11 +1,21 @@
 #[cfg(test)]
-mod tests {
-    use rusty_algoritms::{
-        generate_list,
-        sorting::{
-            bubble_sort::IterativeBubbleSort, merge_sort::RecursiveMergeSort, quick_sort::QuickSort,
-        },
+mod sorting_tests {
+
+    use rusty_algoritms::algorithms::generate_list;
+    use rusty_algoritms::algorithms::sorting::{
+        bubble::{iterative::iterative_bubble_sort, recursive::recursive_bubble_sort},
+        merge::{iterative::iterative_merge_sort, recursive::recursive_merge_sort},
+        quick::{iterative::iterative_quick_sort, recursive::recursive_quick_sort},
+        SortFunction,
     };
+
+    fn make_test(func: SortFunction<i32>) -> (Vec<i32>, Vec<i32>) {
+        let mut random_list = generate_list(1000);
+        let mut random_list_copy = random_list.clone();
+        random_list.sort();
+        func(&mut random_list_copy);
+        (random_list, random_list_copy)
+    }
 
     #[test]
     fn it_works() {
@@ -14,35 +24,50 @@ mod tests {
     }
 
     #[test]
-    fn bubble_sort_test_10x1000() {
+    fn iterative_bubble_test() {
         for _ in 0..10 {
-            let mut random_list = generate_list(1000);
-            let mut random_list_2 = random_list.clone();
-            random_list_2.sort();
-            random_list.iterative_bubble_sort();
-            assert_eq!(random_list, random_list_2)
+            let (l1, l2) = make_test(iterative_bubble_sort);
+            assert_eq!(l1, l2)
         }
     }
 
     #[test]
-    fn quick_sort_test10x1000() {
+    fn recursive_bubble_test() {
         for _ in 0..10 {
-            let mut random_list = generate_list(1000);
-            let mut random_list_2 = random_list.clone();
-            random_list.quick_sort();
-            random_list_2.sort();
-            assert_eq!(random_list, random_list_2);
+            let (l1, l2) = make_test(recursive_bubble_sort);
+            assert_eq!(l1, l2)
         }
     }
 
     #[test]
-    fn merge_sort_test10x1000() {
+    fn iterative_quick_test() {
         for _ in 0..10 {
-            let mut random_list = generate_list(1000);
-            let mut random_list_2 = random_list.clone();
-            random_list.merge_sort();
-            random_list_2.sort();
-            assert_eq!(random_list, random_list_2);
+            let (l1, l2) = make_test(iterative_quick_sort);
+            assert_eq!(l1, l2)
+        }
+    }
+
+    #[test]
+    fn recursive_quick_test() {
+        for _ in 0..10 {
+            let (l1, l2) = make_test(recursive_quick_sort);
+            assert_eq!(l1, l2)
+        }
+    }
+
+    #[test]
+    fn iterative_merge_test() {
+        for _ in 0..10 {
+            let (l1, l2) = make_test(iterative_merge_sort);
+            assert_eq!(l1, l2)
+        }
+    }
+
+    #[test]
+    fn recursive_merge_test() {
+        for _ in 0..10 {
+            let (l1, l2) = make_test(recursive_merge_sort);
+            assert_eq!(l1, l2)
         }
     }
 }
